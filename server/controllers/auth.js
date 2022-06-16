32b2760ab88bcf3d7ef1ccdf4d610485870dbc2d
EDR3X <dhunganaannanta@gmail.com>
2022-06-16 20:54:28 +0545

    Server: fomatting

---
+++ server/controllers/auth.js
@@ -0,0 +1,5 @@
+const getUser = (_req, res) => {
+  res.send("Hello world");
+};
+
+module.exports = { getUser };
--- server/index.js
+++ server/index.js
@@ -1,5 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 5050;
+
+app.use(express.json());
+
+const url = "mongodb://localhost/Amazon-Clone";
+mongoose.connect(url);
+const con = mongoose.connection;
+con.on("open", () => console.log("connected"));
+
+const authRouter = require("./routes/auth");
+app.use("/user", authRouter);
 
 app.listen(PORT, () => `listening to: localhost:${PORT}`);
