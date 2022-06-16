09934f4b71aac3dd18f742e7529c14fd6cac8541
EDR3X <dhunganaannanta@gmail.com>
2022-06-16 16:46:01 +0545

    init

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:amazon_clone/cinstants/global_variables.dart';
+import 'package:flutter/material.dart';
+import 'views/home_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Amazon Clone",
+      theme: ThemeData(
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+        ),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
