ed283ec6a258ead52ff4574aa5c48c96d80ef50f
EDR3X <dhunganaannanta@gmail.com>
2022-07-16 19:43:22 +0545

    state resist added

--- lib/features/auth/services/auth_service.dart
+++ lib/features/auth/services/auth_service.dart
@@ -1,10 +1,14 @@
 import 'dart:convert';
 
+import 'package:amazon_clone/features/home/screen/home_screen.dart';
 import 'package:amazon_clone/models/user.dart';
+import 'package:amazon_clone/providers/user_provider.dart';
 import 'package:amazon_clone/utils/http_error_handler.dart';
 import 'package:amazon_clone/utils/snackbar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:provider/provider.dart';
 import '../../../constants/api.dart';
 
 class AuthService {
@@ -67,15 +71,64 @@ class AuthService {
         },
       );
 
-      print(response.body);
-
       httpErrorHandler(
         response: response,
         context: context,
-        onSuccess: () {},
+        onSuccess: () async {
+          SharedPreferences prefs = await SharedPreferences.getInstance();
+          Provider.of<UserProvider>(context, listen: false)
+              .serUser(response.body);
+          await prefs.setString(
+            'x-auth-token',
+            jsonDecode(response.body)['token'],
+          );
+
+          Navigator.pushNamedAndRemoveUntil(
+            context,
+            HomeScreen.routeName,
+            (route) => false,
+          );
+        },
       );
     } catch (e) {
-      print(e.toString());
+      showSnackBar(context, e.toString());
+    }
+  }
+
+  Future<void> getUserData(
+    BuildContext context,
+  ) async {
+    try {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      String? token = prefs.getString('x-auth-token');
+
+      if (token == null) {
+        prefs.setString('x-auth-token', '');
+      }
+
+      var tokenRes = await http.post(
+        Uri.parse('$apiUrl/api/isTokenValid'),
+        headers: <String, String>{
+          'Content-Type': 'application/json;charset=UTF-8',
+          'x-auth-token': token!
+        },
+      );
+
+      var response = jsonDecode(tokenRes.body);
+
+      if (response == true) {
+        http.Response userRes = await http.get(
+          Uri.parse('$apiUrl/user/'),
+          headers: <String, String>{
+            'Content-Type': 'application/json;charset=UTF-8',
+            'x-auth-token': token,
+          },
+        );
+
+        var userProvider = Provider.of<UserProvider>(context, listen: false);
+        userProvider.serUser(userRes.body);
+      }
+    } catch (e) {
       showSnackBar(context, e.toString());
     }
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,62 @@
+import 'package:amazon_clone/features/auth/services/auth_service.dart';
+import 'package:amazon_clone/features/home/screen/home_screen.dart';
+import 'package:amazon_clone/models/user.dart';
 import 'package:amazon_clone/router.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '/constants/global_variables.dart';
 import '/features/auth/screens/auth_screen.dart';
+import 'providers/user_provider.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final AuthService authService = AuthService();
+
+  @override
+  void initState() {
+    authService.getUserData(context);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Amazon Clone",
-      theme: ThemeData(
-        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
-        colorScheme: const ColorScheme.light(
-          primary: GlobalVariables.secondaryColor,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<UserProvider>(
+          create: (context) => UserProvider(),
         ),
-        appBarTheme: const AppBarTheme(
-          elevation: 0,
-          iconTheme: IconThemeData(
-            color: Colors.black,
+      ],
+      child: Builder(builder: (context) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: "Amazon Clone",
+          theme: ThemeData(
+            scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+            colorScheme: const ColorScheme.light(
+              primary: GlobalVariables.secondaryColor,
+            ),
+            appBarTheme: const AppBarTheme(
+              elevation: 0,
+              iconTheme: IconThemeData(
+                color: Colors.black,
+              ),
+            ),
           ),
-        ),
-      ),
-      home: const AuthScreen(),
-      onGenerateRoute: (settings) => generateRoute(settings),
+          home: Provider.of<UserProvider>(context).user.token.isNotEmpty
+              ? const HomeScreen()
+              : const AuthScreen(),
+          onGenerateRoute: (settings) => generateRoute(settings),
+        );
+      }),
     );
   }
 }
